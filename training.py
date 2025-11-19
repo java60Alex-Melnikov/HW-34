d29f6001ea809ecbc441274b7aa449d979673982
Millenium-Konig <sanyamelnikov097+@gmail.com>
2025-11-19 14:48:17 +0200

    small fix

--- training.py
+++ training.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 
 def train_model():
     model = YOLO('yolov8m.pt')
-    model.train(data='data.yaml', epochs=20, imgsz=640, plots=True)
+    model.train(data='datasets/data.yaml', epochs=20, imgsz=640, plots=True)
     print("Training finished. Model saved to runs/detect/train/weights/best.pt")
 
 if __name__ == '__main__':
